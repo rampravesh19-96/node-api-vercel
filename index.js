845d8374a2f38ee74ab88a072d7d434963cb0525
ram pravesh singh <rampraveshsingh1996@gmail.com>
2023-12-24 01:07:28 +0530

    new

--- index.js
+++ index.js
@@ -1,56 +1,11 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const express = require('express')
+const mysql = require("mysql")
 
-const app = express();
-app.use(bodyParser.json());
-
-// Connect to MongoDB (replace 'mongodb://localhost:27017/mydatabase' with your MongoDB URI)
-mongoose.connect('mongodb+srv://ramps:12345@cluster0.4rtasgf.mongodb.net/test')
-.then(() => {
-  console.log('Connected to MongoDB Atlas');
-})
-.catch((error) => {
-  console.error('Error connecting to MongoDB Atlas:', error.message);
-});
+const app = express()
+const PORT = 4000
 
 
-// Create a user schema
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: { type: String, unique: true },
-  password: String,
-});
 
-// Create a user model
-const User = mongoose.model('User', userSchema);
-
-// Register a new user
-app.post('/register', async (req, res) => {
-  const { name, email, password } = req.body;
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create a new user instance
-    const newUser = new User({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    // Save the user to the database
-    await newUser.save();
-
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Failed to register user' });
-  }
-});
-
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`API listening on PORT ${PORT} `)
 })
@@ -65,6 +20,7 @@ app.get('/about', (req, res) => {
 app.get('/check', (req, res) => {
   res.status(200).json({id : 1, email:"abc@gmail.com",name : "sachin"})
 })
+
 
 // Export the Express API
 module.exports = app
