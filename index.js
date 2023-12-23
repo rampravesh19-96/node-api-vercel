016a2358b220eaf696b69f82a37df91e261f6b9f
rampravesh19-96 <64700001+rampravesh19-96@users.noreply.github.com>
2023-12-23 21:08:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('This is my about route..... ')
 })
-app.get('/check', (req, res) => {
+app.post('/check', (req, res) => {
   res.status(200).json({id : 1, email:"abc@gmail.com",name : "sachin"})
 })
 
