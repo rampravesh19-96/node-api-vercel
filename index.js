622a1e0c9ac35db94705aa701e5bec34d621f5f0
ram pravesh singh <rampraveshsingh1996@gmail.com>
2023-12-28 14:36:40 +0530

    cf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const {getIPAddress} = require("./utils/common");
 require("dotenv").config();
-require("./config/db");
+// require("./config/db");
 // const routes = require("./routes/routes");
 const app = express();
 // app.use(bodyParser.json());
