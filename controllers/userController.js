3131c2ad1b8234c4c26d27cfcb5185fbef06fe50
ram pravesh singh <rampraveshsingh1996@gmail.com>
2023-12-28 15:16:41 +0530

    abc

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,7 +10,7 @@ module.exports = {
   register: async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ errors: errors.array()[0].msg });
     }
 
     const { name, email, password } = req.body;
