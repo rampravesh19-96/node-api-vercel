cdd7110e52537e585a0ea9848d22d11c608ec4b9
ram pravesh singh <rampraveshsingh1996@gmail.com>
2023-12-28 14:25:15 +0530

    check

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const {getIPAddress} = require("./utils/common");
 require("dotenv").config();
-require("./config/db");
-const routes = require("./routes/routes");
+// require("./config/db");
+// const routes = require("./routes/routes");
 const app = express();
-app.use(bodyParser.json());
-app.use(routes);
-
+// app.use(bodyParser.json());
+// app.use(routes);
+app.get("/",(req,res)=>{
+  res.send("hello")
+})
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+  const ipAddress = getIPAddress();
+  console.log(`Server is running on http://${ipAddress}:${process.env.PORT}`);
 });
 module.exports = app
