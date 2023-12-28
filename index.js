c18609b39aed7ee0db3e987f185cdc4bce924e3a
ram pravesh singh <rampraveshsingh1996@gmail.com>
2023-12-28 15:04:53 +0530

    final

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const {getIPAddress} = require("./utils/common");
 require("dotenv").config();
-const mongoose = require("mongoose")
-
-const app = express()
-
-
-app.get("/",(req,res)=>{
-  res.send("hello")
-})
+require("./config/db");
+const routes = require("./routes/routes");
+const app = express();
+app.use(bodyParser.json());
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT} `)
-})
+  const ipAddress = getIPAddress();
+  console.log(`Server is running on http://${ipAddress}:${process.env.PORT}`);
+});
+module.exports = app
