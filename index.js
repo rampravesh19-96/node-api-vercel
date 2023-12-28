27998cb3f1ce87d5f65a3efe45d066778d7f9bca
ram pravesh singh <rampraveshsingh1996@gmail.com>
2023-12-28 14:19:37 +0530

    again

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const {getIPAddress} = require("./utils/common");
 require("dotenv").config();
 require("./config/db");
 const routes = require("./routes/routes");
@@ -9,7 +8,6 @@ app.use(bodyParser.json());
 app.use(routes);
 
 app.listen(process.env.PORT, () => {
-  const ipAddress = getIPAddress();
-  console.log(`Server is running on http://${ipAddress}:${process.env.PORT}`);
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
 module.exports = app
