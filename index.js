2bd6638c089f191d15c46130cba46af7ec9fb0eb
ram pravesh singh <rampraveshsingh1996@gmail.com>
2023-12-28 14:40:32 +0530

    vf

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 const {getIPAddress} = require("./utils/common");
 require("dotenv").config();
 // require("./config/db");
 // const routes = require("./routes/routes");
+mongoose.connect('mongodb+srv://ramps:12345@cluster0.4rtasgf.mongodb.net/custom')
+.then(() => {
+  console.log('Connected to MongoDB Atlas');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB Atlas:', error.message);
+});
 const app = express();
 // app.use(bodyParser.json());
 // app.use(routes);
