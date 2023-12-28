6c9ddef4de81d63c624ef0800a29fa2dc5568579
ram pravesh singh <rampraveshsingh1996@gmail.com>
2023-12-28 14:55:09 +0530

    er

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require("dotenv").config();
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 // require("./config/db")
 
 const app = express()
